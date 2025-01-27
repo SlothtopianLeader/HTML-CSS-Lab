3717b607c25e9f23c936745e1600b7ba5f3429e9
SlothtopianLeader <salyers.audrianna@gmail.com>
2025-01-26 22:47:37 -0500

    Update 1/26/25

--- src/page_posts.py
+++ src/page_posts.py
@@ -40,7 +40,7 @@ div.box {
 div.entry {
     
     border: 1px double black;
-    background:  #cde5e5;
+    /* background:  #cde5e5; */
     padding-top: 0em;
     padding-left: 1em;
     padding-right: 1em;
@@ -71,30 +71,78 @@ div.entry {
     color: black;
     padding: 0.5em;
 }
-
+img{
+    border: 1px double black;
+    margin-top: 0.4em;
+}
 </style>
 <body>
 
 <div class='titlebar' align="center">
-<font color="black" size=30>
-    Posts
+<font color="black" size=40>
+    Your Posts
 </font>
 </div>
 
 <div class="box">
-    <div class="entry">
+    <div class="entry" style="background: #eef6f6">
     <div>
-    <img src="/html/Doge.png" width="64" height="64">
+    <img src="/html/Stitch.png" width="64" height="64">
+    "Anyone have any music recommendations?"
+    4 hours ago
+    Views: 8
     </div>
     </div>
     
-    <div class="entry">
+    <div class="entry" style="background: #ddeeee">
     <div>
-    <img src="/html/Sunset.png" width="64" height="64">
+    <img src="/html/Stitch.png" width="64" height="64">
     </div>
     </div>
     
-    <div class="entry">
+    <div class="entry" style="background: #deeef0">
+    <div>
+    <img src="/html/Stitch.png" width="64" height="64">
+    </div>
+    </div>
+    
+    <div class="entry" style="background: #cde5e5">
+    <div>
+    <img src="/html/Stitch.png" width="64" height="64">
+    </div>
+    </div>
+    
+    <div class="entry" style="background: #bcdcdc">
+    <div>
+    <img src="/html/Stitch.png" width="64" height="64">
+    </div>
+    </div>
+    
+    <div class="entry" style="background: #abd4d4">
+    <div>
+    <img src="/html/Stitch.png" width="64" height="64">
+    </div>
+    </div>
+    
+    <div class="entry" style="background: #9acbcb">
+    <div>
+    <img src="/html/Stitch.png" width="64" height="64">
+    </div>
+    </div>
+    
+    <div class="entry" style="background: #89c2c2">
+    <div>
+    <img src="/html/Stitch.png" width="64" height="64">
+    </div>
+    </div>
+    
+    <div class="entry" style="background: #78baba">
+    <div>
+    <img src="/html/Stitch.png" width="64" height="64">
+    </div>
+    </div>
+    
+    <div class="entry" style="background: #68b1b1">
     <div>
     <img src="/html/Stitch.png" width="64" height="64">
     </div>
